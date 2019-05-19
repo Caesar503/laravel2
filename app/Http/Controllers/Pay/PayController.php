425c6df097c51848eb4a7633883b7b41b760ea34
zhaokai <15201130543@163.com>
2019-05-19 09:51:18 +0000

    test

--- app/Http/Controllers/Pay/PayController.php
+++ app/Http/Controllers/Pay/PayController.php
@@ -94,7 +94,7 @@ class PayController extends Controller
         $aa =rtrim($a,'&');
         //验签
         $res = openssl_verify($aa,base64_decode($sign),$this->aliPubKey,OPENSSL_ALGO_SHA256);
-        dump($res);die;
+        file_put_contents("logs/notify.log",$res.'\n\n\n\n\n',FILE_APPEND);
         if($res){
             //TODO  处理逻辑业务
 
